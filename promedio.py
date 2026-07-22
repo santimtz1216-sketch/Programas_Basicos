7f82d9a3a36c5ab9c3490402a3dbd0e2fa2e5bf6
santimtz1216-sketch <santimtz1216@gmail.com>
2026-07-22 12:26:51 -0600

    Add files via upload

---
+++ promedio.py
@@ -0,0 +1,6 @@
+# Promedio de tres números
+n1 = float(input("Número 1: "))
+n2 = float(input("Número 2: "))
+n3 = float(input("Número 3: "))
+promedio = (n1 + n2 + n3) / 3
+print(f"El promedio es: {promedio}")
